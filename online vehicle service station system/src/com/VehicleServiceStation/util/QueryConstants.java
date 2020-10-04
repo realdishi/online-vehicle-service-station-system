9f1f00b1e74f7f5519a04005936d0a873f631486
ACER <ACER@DESKTOP-BABEV9S>
2020-10-04 09:12:16 +0530

    UPDATE com.VehicleServiceStation.util

---
+++ online vehicle service station system/src/com/VehicleServiceStation/util/QueryConstants.java
@@ -0,0 +1,34 @@
+/**
+ * VEHICLE SERVICE AND FUEL SATATION MANAGEMENT SYSTEM
+ * @author yasith wimukthi
+ * IT 19966922
+ * 
+ * Y2S1 2.2
+ * OOP
+ *
+ */
+
+package com.VehicleServiceStation.util;
+
+public class QueryConstants {
+	
+	/*
+	 * QUERY CONSTANTS FOR DATABASE
+	 */
+	
+	/* Constant for db.properties */
+	public static final String DB_PROPERTY = "db.properties";
+	
+	/* Constant for url to connect database in db.properties */
+	public static final String URL = "url";
+	
+	/* Constant for driver to connect MYSQL in db.properties */
+	public static final String DRIVER_NAME = "driverName";
+	
+	/* Constant for user-name in db.properties*/
+	public static final String USERNAME = "uesrName";
+
+	/* Constant for password in db.properties */
+	public static final String PASSWORD = "password";
+
+}
